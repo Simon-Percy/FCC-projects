5760ac30c0a90164802c2264a2067b2298c8f178
SimonPercy <mmmRaliphada809@gmail.com>
2023-10-18 10:52:11 +0200

    fixed main

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom"; // Correct import
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
