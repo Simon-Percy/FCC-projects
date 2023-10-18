f1dcfcb59e6305a9403a80f222dfe1437e74c6af
SimonPercy <mmmRaliphada809@gmail.com>
2023-10-18 10:39:21 +0200

    added my files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { FaFacebook, FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState({ author: "", content: "" });
+
+  const transition = "all 1s";
+  const colorChange = () => {
+    const red = Math.floor(Math.random() * 128);
+    const green = Math.floor(Math.random() * 128);
+    const blue = Math.floor(Math.random() * 128);
+    return `rgb(${red},${green},${blue})`;
+  };
+  const [randomColor, setrandomColor] = useState(() => colorChange());
+  document.getElementById("root").style.backgroundColor = randomColor;
+  document.getElementById("root").style.transition = transition;
+
+  async function updateQuote() {
+    setrandomColor(colorChange());
+    try {
+      // Fetch a random quote from the Quotable API
+      const response = await fetch("https://api.quotable.io/random");
+      if (response.ok) {
+        const data = await response.json();
+        setQuote(data);
+      } else {
+        console.log(`Error : ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error("Error:fetching data:", error);
+    }
+  }
+  useEffect(() => {
+    updateQuote();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="quote-box" style={{ backgroundColor: "white" }}>
+      <h2 id="text" style={{ color: randomColor, transition }}>
+        <FaQuoteLeft size="30" />
+        {quote.content}
+        <FaQuoteRight size="30" />
+      </h2>
+
+      <p id="author">- {quote.author}</p>
+      <button
+        style={{ backgroundColor: randomColor, transition }}
+        id="new-quote"
+        onClick={() => updateQuote()}
+      >
+        New quote
+      </button>
+      <a id="tweet-quote" href="twitter.com/intent/tweet">
+        <FaXTwitter
+          size="30"
+          style={{
+            color: "white",
+            backgroundColor: randomColor,
+            borderRadius: 8,
+            marginRight: 10,
+            transition,
+          }}
+        />
+        <FaFacebook
+          size="30"
+          style={{
+            color: "white",
+            backgroundColor: randomColor,
+            borderRadius: 8,
+            transition,
+          }}
+        />
+      </a>
+    </div>
+  );
 }
 
-export default App
+export default App;
