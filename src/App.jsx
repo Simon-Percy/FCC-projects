0d215901a81c97a2d7429471323b9164da7b761d
SimonPercy <mmmRaliphada809@gmail.com>
2023-11-03 11:55:11 +0200

    completed project 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const audio = useRef();
-  const [beat, setBeat] = useState("");
   const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
-  const handleClick = (e) => e.target.firstChild.play();
+
+  const refs = keys.map(() => useRef());
+  const [beat, setBeat] = useState("");
+  const handleClick = (e) => {
+    e.target.firstChild.play();
+    setBeat(e.target.id);
+  };
   const handleKeyDown = (e) => {
-    keys.map((key) => {
-      e.key.toUpperCase() === key &&
-        audio.current.play() &&
-        document.getElementById(key).parentElement.focus();
-      setBeat(document.getElementById(e.key).parentElement.id);
+    keys.map((key, index) => {
+      if (e.key.toUpperCase() == key) {
+        refs[index].current.play() && refs[index].current.parentElement.focus();
+        setBeat(refs[index].current.parentElement.id);
+        console.log(refs.current[index]);
+      }
     });
   };
   useEffect(() => {
@@ -23,8 +28,8 @@ function App() {
         {" "}
         <button className="drum-pad" id="Heater 1" onClick={handleClick}>
           <audio
+            ref={refs[0]}
             className="clip"
-            ref="audio"
             src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
             id="Q"
           ></audio>
@@ -33,7 +38,7 @@ function App() {
         <button className="drum-pad" id="Heater 2" onClick={handleClick}>
           <audio
             className="clip"
-            ref="audio"
+            ref={refs[1]}
             src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
             id="W"
           ></audio>
@@ -42,7 +47,7 @@ function App() {
         <button className="drum-pad" id="Heater 3" onClick={handleClick}>
           <audio
             className="clip"
-            ref="audio"
+            ref={refs[2]}
             src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
             id="E"
           ></audio>
@@ -51,7 +56,7 @@ function App() {
         <button className="drum-pad" id="Heater 4" onClick={handleClick}>
           <audio
             className="clip"
-            ref="audio"
+            ref={refs[3]}
             src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
             id="A"
           ></audio>
@@ -60,7 +65,7 @@ function App() {
         <button className="drum-pad" id="Clap" onClick={handleClick}>
           <audio
             className="clip"
-            ref="audio"
+            ref={refs[4]}
             src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
             id="S"
           ></audio>
@@ -69,7 +74,7 @@ function App() {
         <button className="drum-pad" id="Open-HH" onClick={handleClick}>
           <audio
             className="clip"
-            ref="audio"
+            ref={refs[5]}
             src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
             id="D"
           ></audio>
@@ -78,7 +83,7 @@ function App() {
         <button className="drum-pad" id="Kick-n'-Hat" onClick={handleClick}>
           <audio
             className="clip"
-            ref="audio"
+            ref={refs[6]}
             src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
             id="Z"
           ></audio>
@@ -87,7 +92,7 @@ function App() {
         <button className="drum-pad" id="Kick" onClick={handleClick}>
           <audio
             className="clip"
-            ref="audio"
+            ref={refs[7]}
             src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
             id="X"
           ></audio>
@@ -96,7 +101,7 @@ function App() {
         <button className="drum-pad" id="Closed-HH" onClick={handleClick}>
           <audio
             className="clip"
-            ref="audio"
+            ref={refs[8]}
             src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
             id="C"
           ></audio>
