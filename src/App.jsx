86afd70d1e893be105eaf76abf0b5df11a95612b
SimonPercy <mmmRaliphada809@gmail.com>
2023-11-04 21:23:44 +0200

    Got buttons working

--- src/App.jsx
+++ src/App.jsx
@@ -5,77 +5,82 @@ function App() {
   const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
 
   const refs = keys.map(() => useRef());
-  const [beat, setBeat] = useState("");
+  const [equation, setEquation] = useState(0);
   const handleClick = (e) => {
-    e.target.firstChild.play();
-    setBeat(e.target.id);
+    setEquation((prev) => prev + e.target.textContent);
   };
   const handleKeyDown = (e) => {
     keys.map((key, index) => {
       if (e.key.toUpperCase() == key) {
         refs[index].current.play() && refs[index].current.parentElement.focus();
-        setBeat(refs[index].current.parentElement.id);
+        setEquation(refs[index].current.parentElement.id);
       }
     });
   };
+
   useEffect(() => {
     document.addEventListener("keydown", handleKeyDown);
   }, []);
   return (
-    <div id="drum-machine">
-      <div id="drum-box">
-        {" "}
-        <button className="cal-btn" id="Heater 1" onClick={handleClick}>
-          0
-        </button>
-        <button className="cal-btn" id="Heater 2" onClick={handleClick}>
-          1
-        </button>
-        <button className="cal-btn" id="Heater 3" onClick={handleClick}>
-          2
-        </button>
-        <button className="cal-btn" id="Heater 4" onClick={handleClick}>
-          3
-        </button>
-        <button className="cal-btn" id="Clap" onClick={handleClick}>
-          4
-        </button>
-        <button className="cal-btn" id="Open-HH" onClick={handleClick}>
-          5
-        </button>
-        <button className="cal-btn" id="Kick-n'-Hat" onClick={handleClick}>
-          6
-        </button>
-        <button className="cal-btn" id="Kick" onClick={handleClick}>
-          7
-        </button>
-        <button className="cal-btn" id="Closed-HH" onClick={handleClick}>
-          8
-        </button>
-        <button className="cal-btn" id="Closed-HH" onClick={handleClick}>
-          9
-        </button>
-        <button className="cal-btn" id="Closed-HH" onClick={handleClick}>
-          .
-        </button>
-        <button className="cal-btn" id="Closed-HH" onClick={handleClick}>
-          -
-        </button>
-        <button className="cal-btn" id="Closed-HH" onClick={handleClick}>
-          +
-        </button>
-        <button className="cal-btn" id="Closed-HH" onClick={handleClick}>
-          /
-        </button>
-        <button className="cal-btn" id="Closed-HH" onClick={handleClick}>
-          x
-        </button>
-        <button className="cal-btn" id="Closed-HH" onClick={handleClick}>
-          =
-        </button>
+    <>
+      <div id="calulator-box">
+        <div id="display">{equation}</div>
+        <div id="buttons">
+          {" "}
+          <button className="cal-btn" id="zero" onClick={handleClick}>
+            0
+          </button>
+          <button className="cal-btn" id="one" onClick={handleClick}>
+            1
+          </button>
+          <button className="cal-btn" id="two" onClick={handleClick}>
+            2
+          </button>
+          <button className="cal-btn" id="three" onClick={handleClick}>
+            3
+          </button>
+          <button className="cal-btn" id="four" onClick={handleClick}>
+            4
+          </button>
+          <button className="cal-btn" id="five" onClick={handleClick}>
+            5
+          </button>
+          <button className="cal-btn" id="six" onClick={handleClick}>
+            6
+          </button>
+          <button className="cal-btn" id="seven" onClick={handleClick}>
+            7
+          </button>
+          <button className="cal-btn" id="eight" onClick={handleClick}>
+            8
+          </button>
+          <button className="cal-btn" id="nine" onClick={handleClick}>
+            9
+          </button>
+          <button className="cal-btn" id="decimal" onClick={handleClick}>
+            .
+          </button>
+          <button className="cal-btn" id="subtract" onClick={handleClick}>
+            -
+          </button>
+          <button className="cal-btn" id="add" onClick={handleClick}>
+            +
+          </button>
+          <button className="cal-btn" id="divide" onClick={handleClick}>
+            /
+          </button>
+          <button className="cal-btn" id="multiply" onClick={handleClick}>
+            x
+          </button>
+          <button className="cal-btn" id="equals" onClick={handleClick}>
+            =
+          </button>
+          <button className="cal-btn" id="clear" onClick={handleClick}>
+            clear
+          </button>
+        </div>
       </div>
-      <div id="display">{beat}</div>
-    </div>
+    </>
   );
 }
 export default App;
